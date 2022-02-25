45c5a928267c7e6f736d4da549a7c6c786bd88c1
Lauri Kesküll <l.keskull@student.tudelft.nl>
2022-02-25 11:17:20 +0100

    Trying to make an endpoint for the "Game" entity Added Game package under server.
    Added the unversioned config, repository and service.

---
+++ server/src/main/java/server/Game/GameConfig.java
@@ -0,0 +1,28 @@
+package server.Game;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class GameConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(GameRepository repository) {
+        return args -> {
+            Game game1 = new Game(
+                    "Kuba",
+                    10
+            );
+
+            Game game2 = new Game(
+                    "Lauri",
+                    11
+            );
+
+            repository.saveAll(List.of(game1, game2));
+        };
+    }
+}
---
+++ server/src/main/java/server/Game/GameRepository.java
@@ -0,0 +1,7 @@
+package server.Game;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository <Game, Integer> {
+
+}
