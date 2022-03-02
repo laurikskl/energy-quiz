767c5c18ca0d18d33b99f9dcb29fd379d6ada8c6
Lauri Kesküll <l.keskull@student.tudelft.nl>
2022-03-02 15:18:39 +0100

    Added the Autowired tags to GameController

--- server/src/main/java/server/Game/GameController.java
+++ server/src/main/java/server/Game/GameController.java
@@ -2,6 +2,7 @@ package server.Game;
 
 
 import commons.Game;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,11 @@ public class GameController {
 
     private final GameService gameService;
 
+    @Autowired
     public GameController(GameService gameService){
         this.gameService = gameService;
     }
+
     @GetMapping
     public List<Game> getGames() {
         return gameService.getGames();
