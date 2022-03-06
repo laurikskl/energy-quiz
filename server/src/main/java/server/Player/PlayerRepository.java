27a4c631d66056458c80aaf7f11b4bfbe22465d0
Alexandra Căruțașu <A.Carutasu@student.tudelft.nl>
2022-03-06 15:09:15 +0100

    Removed unused imports from PlayerRepository.

--- server/src/main/java/server/Player/PlayerRepository.java
+++ server/src/main/java/server/Player/PlayerRepository.java
@@ -18,7 +18,4 @@ package server.Player;
 import commons.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface PlayerRepository extends JpaRepository<Player, String> {}
