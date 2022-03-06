ed87a97d26be5eead3b9c2abbb9e0d952e2ecb6d
Alexandra Căruţaşu <a.carutasu@student.tudelft.nl>
2022-03-06 11:59:41 +0100

    Revert "Added a Player package, with the controller and the repository for the player entity."
    This reverts commit 345542b51846e1de089b1b36705579ee4fe7ca85.

--- server/src/Player/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package server.Player;
-
-import commons.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, String> {
-}
