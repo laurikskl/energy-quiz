345542b51846e1de089b1b36705579ee4fe7ca85
Alexandra Căruţaşu <a.carutasu@student.tudelft.nl>
2022-03-06 11:59:41 +0100

    Added a Player package, with the controller and the repository for the player entity.

---
+++ server/src/Player/PlayerRepository.java
@@ -0,0 +1,7 @@
+package server.Player;
+
+import commons.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<Player, String> {
+}
