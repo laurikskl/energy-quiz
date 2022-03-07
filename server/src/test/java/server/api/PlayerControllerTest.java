59821ed3f939048e8a00635d924ddaeb08f8dfbe
Alexandra Căruțașu <A.Carutasu@student.tudelft.nl>
2022-03-07 12:43:12 +0100

    Removed other unused imports from my files.

--- server/src/test/java/server/api/PlayerControllerTest.java
+++ server/src/test/java/server/api/PlayerControllerTest.java
@@ -1,17 +1,13 @@
 package server.api;
 
-import commons.Person;
 import commons.Player;
-import commons.Quote;
 import server.Player.PlayerController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import server.Player.PlayerService;
 
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class PlayerControllerTest {
