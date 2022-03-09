21103c224935a98e7cb33f58766f44aa3bd0348e
Jakub Kontak <J.Kontak@student.tudelft.nl>
2022-03-09 19:01:26 +0100

    Added comments for the tests to make them clearer and some additional tests

--- commons/src/test/java/commons/ActivityTest.java
+++ commons/src/test/java/commons/ActivityTest.java
@@ -10,50 +10,93 @@ public class ActivityTest {
     Activity activity;
     Activity activityChanged;
 
+    /**
+     * Initializing activity and activityChange for the rest of the tests
+     */
     @BeforeEach
     public void initialize() {
         activity = new Activity("Cycling", 420);
         activityChanged = new Activity("Cycling", 420);
     }
 
+    /**
+     * Testing if the constructor worked
+     */
     @Test
     public void constructorTest() {
         assertNotNull(activity);
     }
 
+    /**
+     * Testing if the id of the 1st activity is 0
+     */
     @Test
-    public void getIdTest() {
-        assertEquals(0 ,activity.getId());
+    public void getIdTest1() {
+        assertEquals(0, activity.getId());
     }
 
+    /**
+     * Testing getter for the name
+     */
     @Test
     public void getNameTest() {
         assertEquals("Cycling", activity.getName());
     }
 
+    /**
+     * Testing getter for the power consumption
+     */
     @Test
     public void getPowerConsumptionTest() {
         assertEquals(420, activity.getPowerConsumption());
     }
 
+    /**
+     * Testing if setting Id to 2 works
+     */
     @Test
     public void setIdTest() {
         activityChanged.setId(2);
         assertEquals(2, activityChanged.getId());
     }
 
+    /**
+     * Testing if seting name to "Biking" works
+     */
     @Test
     public void setNameTest() {
         activityChanged.setName("Biking");
         assertEquals("Biking", activityChanged.getName());
     }
 
+    /**
+     * Testing if setting power consumption to 666 works
+     */
     @Test
     public void setPowerConsumptionTest() {
         activityChanged.setPowerConsumption(666);
         assertEquals(666, activityChanged.getPowerConsumption());
     }
 
+    /**
+     * Testing if equals method works for the same reference
+     */
+    @Test
+    public void equalsSameObjectTest() {
+        assertTrue(activity.equals(activity));
+    }
+
+    /**
+     * Testing if equals method works when comparing with null
+     */
+    @Test
+    public void equalsNullTest() {
+        assertFalse(activity.equals(null));
+    }
+
+    /**
+     * Testing if equals method works when id is
+     */
     @Test
     public void equalsTest() {
         Activity activitySame = new Activity("Cycling", 420);
@@ -64,12 +107,12 @@ public class ActivityTest {
         assertFalse(activityDiff.equals(activity));
     }
 
+    /**
+     * Testing if toString method works
+     */
     @Test
     public void toStringTest() {
-        assertEquals("Activity{" +
-                "id=" + 0 +
-                ", name='" + "Cycling" + '\'' +
-                ", powerConsumption=" + 420 +
-                '}', activity.toString());
+        assertEquals("Activity{id=" + 0 + ", name='Cycling', powerConsumption=" + 420 + '}'
+                    , activity.toString());
     }
 }
