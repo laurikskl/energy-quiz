67c659608a142a679999222261eaf6c045af0ba7
Alexandra Căruțașu <A.Carutasu@student.tudelft.nl>
2022-03-10 15:30:59 +0100

    Removed an unused import from a file.

--- server/src/main/java/server/Player/PlayerController.java
+++ server/src/main/java/server/Player/PlayerController.java
@@ -1,7 +1,6 @@
 package server.Player;
 
 import commons.Player;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
