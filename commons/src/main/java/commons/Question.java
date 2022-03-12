bec73f53bb0bd4778deb9b938046681a85454f4f
Lauri Kesküll <L.Keskull@student.tudelft.nl>
2022-03-12 12:45:14 +0100

    Abstract question class for the 4 different subtypes of:
    Multiple choice
    Estimation with 3 choices
    Matching an activity to another one that consumes the same amount
    Estimation with open field

---
+++ commons/src/main/java/commons/Question.java
@@ -0,0 +1,56 @@
+package commons;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public abstract class Question {
+
+  /**
+   * Question where the player must choose the activity with the highest energyConsumption
+   * out of 3 available options.
+   */
+
+  @AllArgsConstructor
+  @Getter
+  public static class MultiChoice extends Question {
+    private final List<Activity> activities;
+    private final int consumptionWh;
+
+  }
+  /**
+   * Question where the player must choose the correct amount of consumption in Wh out of 3
+   * different options for a given activity.
+   */
+
+  @AllArgsConstructor
+  @Getter
+  public static class ChoiceEstimation extends Question {
+    private final Activity activity;
+    private final int consumptionWh;
+  }
+  /**
+   * Question where the player get an activity like "Taking a shower (50l water)" and must
+   * select an activity that consumes an equivalent amount of energy out of 3 options.
+   */
+
+  @AllArgsConstructor
+  @Getter
+  public static class Matching extends Question {
+    private final List<Activity> activities;
+    private final int consumptionWh;
+
+  }
+  /**
+   * Question with x possible activities to choose, one of them is the correct answer
+   */
+
+  @AllArgsConstructor
+  @Getter
+  public static class AccurateEstimation extends Question {
+    private final Activity activity;
+    private final int consumptionWh;
+  }
+
+
+}
