67ca252537dae8b820664dcd52a5fa5d5ea0df71
Alexandra Căruțașu <A.Carutasu@student.tudelft.nl>
2022-03-16 14:46:16 +0100

    Removed unused imports.

--- client/src/main/java/client/scenes/SplashCtrl.java
+++ client/src/main/java/client/scenes/SplashCtrl.java
@@ -6,11 +6,9 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -19,7 +17,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
-import java.util.ResourceBundle;
 
 public class SplashCtrl {
 
