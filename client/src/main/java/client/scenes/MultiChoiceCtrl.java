7999cee4dfdfcb0fb2cfd5be249e8771dec9536a
Lauri Kesküll <L.Keskull@student.tudelft.nl>
2022-03-19 11:47:13 +0100

    Added functionality to the buttons so the right and wrong answers are displayed

--- client/src/main/java/client/scenes/MultiChoiceCtrl.java
+++ client/src/main/java/client/scenes/MultiChoiceCtrl.java
@@ -3,6 +3,7 @@ package client.scenes;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import commons.Question;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -38,7 +39,7 @@ public class MultiChoiceCtrl {
   }
 
   @FXML
-  public void initialize() {
+  public void onOpen() {
     image1.setImage(new Image(multiChoice.getActivities().get(0).getImagePath()));
     image2.setImage(new Image(multiChoice.getActivities().get(1).getImagePath()));
     image3.setImage(new Image(multiChoice.getActivities().get(2).getImagePath()));
@@ -47,10 +48,26 @@ public class MultiChoiceCtrl {
     answer3.setText(multiChoice.getActivities().get(2).getName());
   }
 
-
-
-  public void onButtonClick() {
+  @FXML
+  public void handleButtonPress(ActionEvent actionEvent) {
+    Button correct = answer1;
+    Button wrong1 = answer2;
+    Button wrong2 = answer3;
+    if(answer2.getText().equals(multiChoice.getCorrect().getName())){
+      correct = answer2;
+      wrong1 = answer1;
+      wrong2 = answer3;
+    }
+    else if(answer3.getText().equals(multiChoice.getCorrect().getName())){
+      correct = answer3;
+      wrong1 = answer1;
+      wrong2 = answer2;
+    }
+    correct.setStyle("-fx-background-color: #00FF00; ");
+    wrong1.setStyle("-fx-background-color: #FF0000; ");
+    wrong2.setStyle("-fx-background-color: #FF0000; ");
+  }
 
   }
 
-}
+
