c19ebe667a05d220012f1558dcf343045c47234c
Lauri Kesküll <L.Keskull@student.tudelft.nl>
2022-03-23 17:29:16 +0100

    - Added the config and dependencies for websockets. - Added the serverutils methods for websockets. - Created a GameManagement package on the serverside that generates new games and gets games by their id, if no id like the one inserted is available then a new game is created. - Added a way to create a new game.

--- server/src/main/java/server/GameManagement/GameManagementController.java
+++ server/src/main/java/server/GameManagement/GameManagementController.java
@@ -10,7 +10,20 @@ public class GameManagementController {
     this.gameManagementService = gameManagementService;
   }
 
+  /**
+   * gets a game by its id.
+   * @param id the id of the game we are getting
+   * @return the game with the correct id, or a new game
+   */
   public Game getGame(long id){
     return gameManagementService.getById(id);
+  }
+
+  /**
+   * starts a new game.
+   * @return A new game
+   */
+  public Game startGame(){
+    return gameManagementService.startLobby();
   }
 }
--- server/src/main/java/server/GameManagement/GameManagementService.java
+++ server/src/main/java/server/GameManagement/GameManagementService.java
@@ -18,6 +18,11 @@ public class GameManagementService {
     this.generateId = 0;
   }
 
+  /**
+   * fetches a game by its id.
+   * @param id the id of the game were looking for
+   * @return returns the game with the id or if no such game exists, a new one
+   */
   public Game getById(long id){
     Game game = null;
 
@@ -35,11 +40,16 @@ public class GameManagementService {
     return game;
   }
 
+  /**
+   * Starts a new game with a unique id.
+   * @return fresh game with unique id and empty players and questions lists
+   */
   public Game startLobby(){
     ArrayList<Player> players = new ArrayList<>();
     ArrayList<Question> questions = new ArrayList<>();
     Game newGame = new Game(generateId, players, questions);
     generateId++;
+    games.add(newGame);
     return newGame;
   }
 }
