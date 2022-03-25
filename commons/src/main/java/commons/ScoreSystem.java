870640212db49d8b77bf6c7f612fd575d2a86afa
Ioana Demetra Carata Dejoianu <I.D.CarataDejoianu@student.tudelft.nl>
2022-03-25 19:47:53 +0100

    Made a "smart" score system and tests

--- commons/src/main/java/commons/ScoreSystem.java
+++ commons/src/main/java/commons/ScoreSystem.java
@@ -2,8 +2,8 @@ package commons;
 
 public class ScoreSystem {
     /**
-     * Placeholder method for counting points in "normal" (choice) questions:
-     * Most Energy, ChoiceEstiamate, Matching
+     * Method for counting points in "normal" (choice) questions:
+     * Most Energy, ChoiceEstimate, Matching
      *
      * @param time spent on a question
      * @return
@@ -13,13 +13,34 @@ public class ScoreSystem {
     }
 
     /**
-     * Placeholder method for counting points in AccurateEstimation questions
+     * Method for counting points in AccurateEstimation questions
      *
      * @param time     spent on a question
-     * @param distance to correct answer
+     * @param actualAnswer  answer to the question
+     * @param correctAnswer correct answer
      * @return
      */
-    public static int calculateScore(Long time, Long distance) {
-        return (int) ((100 - time) * 10 - distance * 10);
+    public static int calculateScore(Long time, Long actualAnswer, Long correctAnswer) {
+        //we calculate the score to reward players that are closer to the correct value,
+        //by calculating what percentage of the correct answer is the actual answer
+        //anything above 200% doesn't give points
+        int percentage;
+        percentage = (int) ((actualAnswer * 100)/correctAnswer);
+        if (percentage > 100) percentage = 200 - percentage;
+        if (percentage < 0) percentage = 0;
+
+        //implementing a multiplier, so when the answer and correct value are both small numbers (max. 3 digits),
+        //having more than 200% difference is ok
+        int multiplier = 0;
+        if (String.valueOf(correctAnswer).length() == String.valueOf(actualAnswer).length()
+            && correctAnswer != actualAnswer)
+            multiplier = 4 - String.valueOf(correctAnswer).length();
+        if (multiplier < 0) multiplier = 0;
+        
+        if ((percentage * percentage + 1000 * multiplier < 10000) && percentage == 0)
+            return (int) ((100 - time) * (1000 * multiplier))/1000;
+
+        //we raise to the power of 2, so the closer you get, the more points you get
+        return (int) ((100 - time) * (percentage * percentage))/1000;
     }
 }
