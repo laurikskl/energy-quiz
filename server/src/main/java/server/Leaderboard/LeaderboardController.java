56cacab5bd5f582de7b204cfd828e410c35e48e2
Alexandra Căruțașu <A.Carutasu@student.tudelft.nl>
2022-04-02 15:34:15 +0200

    Removed unused imports.

--- server/src/main/java/server/Leaderboard/LeaderboardController.java
+++ server/src/main/java/server/Leaderboard/LeaderboardController.java
@@ -3,12 +3,10 @@ package server.Leaderboard;
 import commons.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import server.database.PlayerRepository;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
