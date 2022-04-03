f92d43991cd9ff74a2d93b6cf65e1954f02347c1
Alexandra Căruțașu <A.Carutasu@student.tudelft.nl>
2022-04-03 20:09:13 +0200

    Remove unused import.

--- client/src/main/java/client/scenes/SPGameCtrl.java
+++ client/src/main/java/client/scenes/SPGameCtrl.java
@@ -26,7 +26,6 @@ import javax.swing.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
